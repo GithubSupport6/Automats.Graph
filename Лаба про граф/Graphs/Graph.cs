dcaa609d3c99822b075a2152580fe77da7868c04
class <class@PC2-310-5>
2019-04-15 10:42:17 +0300

    Добавьте файлы проекта.

---
+++ Лаба про граф/Graphs/Graph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Лаба_про_граф.Graphs;
+
+namespace Лаба_про_граф
+{
+    class Graph
+    {
+        LinkedList<Node> nodes
+        {
+            get;
+        }
+
+        Node root
+        {
+            get;
+        }
+
+
+    }
+}
---
+++ Лаба про граф/Graphs/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба_про_граф.Graphs
+{
+    class Node
+    {
+        public LinkedList<KeyValuePair<String, Node>> links
+        {
+            get;
+        }
+
+        public String data
+        {
+            get;
+        }
+
+        public Node(String data)
+        {
+            links = new LinkedList<KeyValuePair<string, Node>>();
+        }
+    }
+}
